64d44a415fb95d35b72d32c7671b9beee65eebe8
Santiago Báez Duic <93755514+SantiagoBaezDuic@users.noreply.github.com>
2024-05-11 02:40:33 -0300

    Finished 1.2 API

--- FunctionsLibrary/src/biblioteca/funciones/tokens.hpp
+++ FunctionsLibrary/src/biblioteca/funciones/tokens.hpp
@@ -31,20 +31,72 @@ void addToken(string &s, char sep, string t)
 
 string getTokenAt(string s, char sep, int i)
 {
-   return "";
+   string token;
+   if (i == 0)
+   {
+      token = substring(s, 0, indexOf(s, sep));
+   }
+   else
+   {
+      int ini = indexOfN(s, sep, i) + 1;
+      int end = indexOfN(s, sep, i + 1);
+      token = substring(s, ini, end);
+   }
+
+   return token;
 }
 
 void removeTokenAt(string &s, char sep, int i)
 {
+   if (i == 0)
+   {
+      s = substring(s, indexOf(s, sep) + 1);
+   }
+   else
+   {
+      int ini = indexOfN(s, sep, i);
+      int end = indexOfN(s, sep, i + 1);
+      string subs1 = substring(s, 0, ini);
+      string subs2 = substring(s, end, length(s));
+      s = subs1 + subs2;
+   }
 }
 
 void setTokenAt(string &s, char sep, string t, int i)
 {
+   if (i >= tokenCount(s, sep))
+   {
+      s += sep + t;
+   }
+   else
+   {
+      if (i == 0)
+      {
+         s = t + sep + substring(s, indexOf(s, sep) + 1);
+      }
+      else
+      {
+         int ini = indexOfN(s, sep, i);
+         int end = indexOfN(s, sep, i + 1);
+         string subs1 = substring(s, 0, ini + 1);
+         string subs2 = substring(s, end, length(s));
+         s = subs1 + t + subs2;
+      }
+   }
 }
 
 int findToken(string s, char sep, string t)
 {
-   return 0;
+   int pos = -1;
+   for (int i = 0; i < tokenCount(s, sep); i++)
+   {
+      if (getTokenAt(s, sep, i) == t)
+      {
+         pos = i;
+      }
+      }
+
+   return pos;
 }
 
 #endif
--- FunctionsLibrary/src/principal.cpp
+++ FunctionsLibrary/src/principal.cpp
@@ -16,20 +16,23 @@ using namespace std;
 
 int main()
 {
-   string s = "";
+   string s = "John|Paul|George|Ringo";
    char sep = '|';
-
-   addToken(s, sep, "John");
-   cout << s << endl; // Salida: John
-
-   addToken(s, sep, "Paul");
-   cout << s << endl; // Salida: John|Paul
-
-   addToken(s, sep, "George");
-   cout << s << endl; // Salida: John|Paul|George
-
-   addToken(s, sep, "Ringo");
-   cout << s << endl; // Salida: John|Paul|George|Ringo
+   string t = "Paul";
+   int p = findToken(s, sep, t);
+   cout << p << endl; // Salida: 1
+   string x = "John";
+   p = findToken(s, sep, x);
+   cout << p << endl; // Salida: 0
+   string o = "George";
+   p = findToken(s, sep, o);
+   cout << p << endl; // Salida: 2
+   string r = "Ringo";
+   p = findToken(s, sep, r);
+   cout << p << endl; // Salida: 3
+   string q = "Pablo";
+   p = findToken(s, sep, q);
+   cout << p << endl; // Salida: -1
 
    return 0;
 }
